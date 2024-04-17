9f077d49fe2e189c3d37e16a690bea142b8bfa5b
svramprabu <svramprabu@gmail.com>
2024-04-17 17:18:46 +0530

    initial commit for day 1

---
+++ airplane_mode/airplane_mode/doctype/airplane/test_airplane.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Ramprabu S V (SVR) and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestAirplane(FrappeTestCase):
+	pass
